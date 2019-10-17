9d497ee9d88f1a560192554a684a5fb82e233458
Yujith Isura <yujith@Yujiths-MacBook-Pro.local>
2019-10-17 22:32:03 +0530

    pdf server error testing 5

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ var options = {
     // "zoomFactor": "0.5"
   };
 // const fs = require('fs');
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
 
 const pdfTemplate = require('./documents');
 const PresidentialPdfTemplate = require('./documents/presidential_nomination_form_sin.js');
