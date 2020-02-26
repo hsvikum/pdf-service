deffeb27c3a8d1297d0da236cc51ed20022ebbe7
Shalitha Vikum <hsvikum@gmail.com>
2020-02-26 06:34:28 +0530

    Added a fix for puppeteer crashing due to low memory

--- puppeteer.js
+++ puppeteer.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const config = require('./config');
 const headlessState = true;
 
-puppeteer.launch({ headless: headlessState, args: ['--no-sandbox'] }).then(function(browser) {
+puppeteer.launch({ headless: headlessState, args: ['--no-sandbox', '--disable-dev-shm-usage'] }).then(function(browser) {
     global.browser = browser;
     console.log('browser ready');
 });
